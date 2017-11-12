e70c3575f9c0834f939dc758f4e4b594347fdda3
zhipengzou <zhipengzou@caijing.com.cn>
2017-11-12 10:06:52 +0800

    fuck

--- config/database.php
+++ config/database.php
@@ -67,9 +67,9 @@ return [
             'sslmode' => 'prefer',
         ],
         'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', 'localhost'),
-            'port'     => env('DB_PORT', 27017),
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', 27017),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
