6b9f413e50f268354fb4255c7bc03d20591c974a
zhipengzou <zhipengzou@caijing.com.cn>
2017-11-13 00:09:53 +0800

    去掉视频

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -114,6 +114,26 @@
             // 隐藏“网络图片”tab
             editor.customConfig.showLinkImg = false;
             editor.customConfig.zIndex = 1;
+            editor.customConfig.menus = [
+                'head',  // 标题
+                'bold',  // 粗体
+                'italic',  // 斜体
+                'underline',  // 下划线
+                'strikeThrough',  // 删除线
+                'foreColor',  // 文字颜色
+                'backColor',  // 背景颜色
+                'link',  // 插入链接
+                'list',  // 列表
+                'justify',  // 对齐方式
+                'quote',  // 引用
+                'emoticon',  // 表情
+                'image',  // 插入图片
+                'table',  // 表格
+//                'video',  // 插入视频
+                'code',  // 插入代码
+                'undo',  // 撤销
+                'redo'  // 重复
+            ];
             editor.create();
 
             $('button.btn-save').click(function () {
